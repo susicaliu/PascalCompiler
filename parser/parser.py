45f7ea36df5a5900aff1c735292abb4190a9a9b4
susicaliu <shliu@zju.edu.cn>
2020-05-20 18:30:07 +0800

    change tokens in parser and fix bugs

--- parser/parser.py
+++ parser/parser.py
@@ -2,11 +2,11 @@ import ply.yacc as yacc
 from scanner.lex import tokens
 
 def p_program(p):
-    'program : program_head  routine  DOT'
+    'program : program_head  routine  SYM_PERIOD'
     p[0] = ProgramNode(p[1],p[2])
 
 def p_program_head(p):
-    'program_head : PROGRAM  ID  SEMI'
+    'program_head : PAS_PROGRAM  ID  SYM_SEMICOLON'
     p[0] = ProgramHeadNode(p[2])
 
 def p_routine(p):
@@ -26,7 +26,7 @@ def p_label_part(p):
     p[0] = None
 
 def p_const_part_0(p):
-    'const_part : CONST  const_expr_list'
+    'const_part : PAS_CONST  const_expr_list'
     p[0] = p[2]
 
 def p_const_part_1(p):
@@ -34,17 +34,17 @@ def p_const_part_1(p):
     p[0] = None
 
 def p_const_expr_list_0(p):
-    'const_expr_list : const_expr_list  ID  EQUAL  const_value  SEMI'
+    'const_expr_list : const_expr_list  ID  SYM_EQ  const_value  SYM_SEMICOLON'
     p[0] = p[1]
     p[0].append(ConstExprNode(VariableNode(p[2]),p[4]))
 
 def p_const_expr_list_1(p):
-    'const_expr_list : ID  EQUAL  const_value  SEMI'
-    p[0] = ConstExprListNode(ConstExprNode(p[1],p[3]))
+    'const_expr_list : ID  SYM_EQ  const_value  SYM_SEMICOLON'
+    p[0] = ConstExprListNode(ConstExprNode(VariableNode(p[1]),p[3]))
 
 def p_const_value_0(p):
-    'const_value : INTEGER'
-    p[0] = ConstValueNode('integer',p[1]);
+    'const_value : INT'
+    p[0] = ConstValueNode('int',p[1]);
 
 def p_const_value_1(p):
     'const_value : REAL'
@@ -55,15 +55,15 @@ def p_const_value_2(p):
     p[0] = ConstValueNode('char',p[1]);
 
 def p_const_value_3(p):
-    'const_value : STRING'
-    p[0] = ConstValueNode('string',p[1]);
+    'const_value : STR'
+    p[0] = ConstValueNode('str',p[1]);
 
 def p_const_value_4(p):
-    'const_value : SYS_CON'
+    'const_value : SYS_CON' ###
     p[0] = ConstValueNode('syscon',p[1]);###
 
 def p_type_part_0(p):
-    'type_part : TYPE type_decl_list'
+    'type_part : PAS_TYPE type_decl_list'
     p[0] = p[2]
 
 def p_type_part_1(p):
@@ -80,7 +80,7 @@ def p_type_decl_list_1(p):
     p[0] = TypeDeclListNode(p[1])
 
 def p_type_definition(p):
-    'type_definition : ID  EQUAL  type_decl  SEMI'
+    'type_definition : ID  SYM_EQ  type_decl  SYM_SEMICOLON'
     p[0] = TypeDefinitionNode(p[1],p[3])
 
 def p_type_decl_0(p):
@@ -104,31 +104,31 @@ def p_simple_type_decl_1(p):
     p[0] = VariableTypeDeclNode(p[1])
 
 def p_simple_type_decl_2(p):
-    'simple_type_decl : LP  name_list  RP '
+    'simple_type_decl : SYM_LPAREN  name_list  SYM_RPAREN '
     p[0] = EnumTypeDeclNode(p[2])
 
 def p_simple_type_decl_3(p):
-    'simple_type_decl : const_value  DOTDOT  const_value  '
+    'simple_type_decl : const_value  SYM_RANGE  const_value  '
     p[0] = RangeTypeDeclNode(1,p[1],1,p[3])
 
 def p_simple_type_decl_4(p):
-    'simple_type_decl : MINUS  const_value  DOTDOT  const_value'
+    'simple_type_decl : SYM_SUB  const_value  SYM_RANGE  const_value'
     p[0] = RangeTypeDeclNode(-1,p[2],1,p[4])
 
 def p_simple_type_decl_5(p):
-    'simple_type_decl : MINUS  const_value  DOTDOT  MINUS  const_value'
+    'simple_type_decl : SYM_SUB  const_value  SYM_RANGE  SYM_SUB  const_value'
     p[0] = RangeTypeDeclNode(-1,p[2],-1,p[4])
 
 def p_simple_type_decl_6(p):
-    'simple_type_decl : ID  DOTDOT  ID'
+    'simple_type_decl : ID  SYM_RANGE  ID'
     pass###
 
 def p_array_type_decl(p):
-    'array_type_decl : ARRAY  LB  simple_type_decl  RB  OF  type_decl'
+    'array_type_decl : PAS_ARRAY  SYM_LBRAC  simple_type_decl  SYM_RBRAC  PAS_OF  type_decl'
     p[0] = ArrayTypeDeclNode(p[3],p[6])
 
 def p_record_type_decl(p):
-    'record_type_decl : RECORD  field_decl_list  END'
+    'record_type_decl : PAS_RECORD  field_decl_list  PAS_END'
     p[0] = RecordTypeDeclNode(p[2])
 
 def p_field_decl_list_0(p):
@@ -141,11 +141,11 @@ def p_field_decl_list_1(p):
     p[0] = FieldDeclListNode(p[1])
 
 def p_field_decl(p):
-    'field_decl : name_list  COLON  type_decl  SEMI'
+    'field_decl : name_list  SYM_COLON  type_decl  SYM_SEMICOLON'
     pass ###
 
 def p_name_list_0(p):
-    'name_list : name_list  COMMA  ID'
+    'name_list : name_list  SYM_COMMA  ID'
     p[0] = p[1]
     p[0].append(p[3])
 
@@ -154,7 +154,7 @@ def p_name_list_1(p):
     p[0] = NameListNode(p[1])
 
 def p_var_part_0(p):
-    'var_part : VAR  var_decl_list'
+    'var_part : PAS_VAR  var_decl_list'
     p[0] = p[2]
 
 def p_var_part_1(p):
@@ -171,7 +171,7 @@ def p_var_decl_list_1(p):
     p[0] = VarDeclListNode(p[1])
 
 def p_var_decl(p):
-    'var_decl :  name_list  COLON  type_decl  SEMI'
+    'var_decl :  name_list  SYM_COLON  type_decl  SYM_SEMICOLON'
     p[0] = VarDeclNode(p[1],p[3])
 
 def p_routine_part_0(p):
@@ -197,23 +197,23 @@ def p_routine_part_4(p):
     p[0] = None
 
 def p_function_decl(p):
-    'function_decl : function_head  SEMI  sub_routine  SEMI'
-    p[0] = FuntionDeclNode(p[1],p[3])
+    'function_decl : function_head  SYM_SEMICOLON  sub_routine  SYM_SEMICOLON'
+    p[0] = FunctionDeclNode(p[1],p[3])
 
 def p_function_head(p):
-    'function_head :  FUNCTION  ID  parameters  COLON  simple_type_decl '
+    'function_head :  PAS_FUNCTION  ID  parameters  SYM_COLON  simple_type_decl '
     p[0] = FunctionHeadNode(p[2],p[3],p[5])
 
 def p_procedure_decl(p):
-    'procedure_decl :  procedure_head  SEMI  sub_routine  SEMI'
+    'procedure_decl :  procedure_head  SYM_SEMICOLON  sub_routine  SYM_SEMICOLON'
     p[0] = ProcedureDeclNode(p[1],p[3])
 
 def p_procedure_head(p):
-    'procedure_head :  PROCEDURE ID parameters '
+    'procedure_head : PAS_PROCEDURE ID parameters '
     p[0] = ProcedureHeadNode(p[2],p[3])
 
 def p_parameters_0(p):
-    'parameters : LP  para_decl_list  RP'
+    'parameters : SYM_LPAREN  para_decl_list  SYM_RPAREN'
     p[0] = p[2]
 
 def p_parameters_1(p):
@@ -221,7 +221,7 @@ def p_parameters_1(p):
     p[0] = None
 
 def p_para_decl_list_0(p):
-    'para_decl_list : para_decl_list  SEMI  para_type_list'
+    'para_decl_list : para_decl_list  SYM_SEMICOLON  para_type_list'
     p[0] = p[1]
     p[0].append(p[3])
 
@@ -230,15 +230,15 @@ def p_para_decl_list_1(p):
     p[0] = ParaDeclListNode(p[1])
 
 def p_para_type_list_0(p):
-    'para_type_list : var_para_list COLON  simple_type_decl  '
+    'para_type_list : var_para_list SYM_COLON  simple_type_decl  '
     p[0] = ParaTypeListNode(p[1],p[3])
 
 def p_para_type_list_1(p):
-    'para_type_list : val_para_list  COLON  simple_type_decl'
+    'para_type_list : val_para_list  SYM_COLON  simple_type_decl'
     p[0] = ParaTypeListNode(p[1],p[3])
 
 def p_var_para_list(p):
-    'var_para_list : VAR  name_list'
+    'var_para_list : PAS_VAR  name_list'
     p[0] = p[2]
 
 def p_val_para_list(p):
@@ -250,11 +250,11 @@ def p_routine_body(p):
     p[0] = p[1]
 
 def p_compound_stmt(p):
-    'compound_stmt : BEGIN  stmt_list  END'
+    'compound_stmt : PAS_BEGIN  stmt_list  PAS_END'
     p[0] = p[2]
 
 def p_stmt_list_0(p):
-    'stmt_list : stmt_list  stmt  SEMI'
+    'stmt_list : stmt_list  stmt  SYM_SEMICOLON'
     if p[1] is not None:
         p[0] = p[1]
         p[0].append(p[2])
@@ -266,7 +266,7 @@ def p_stmt_list_1(p):
     p[0] = None
 
 def p_stmt_0(p):
-    'stmt : INTEGER  COLON  non_label_stmt'
+    'stmt : INT  SYM_COLON  non_label_stmt'
     p[0] = p[3]
     p[0].set_id(p[1])###
 
@@ -311,15 +311,15 @@ def p_non_label_stmt_8(p):
     p[0] = p[1]
 
 def p_assign_stmt_0(p):
-    'assign_stmt : ID  ASSIGN  expression'
+    'assign_stmt : ID  SYM_ASSIGN  expression'
     p[0] = AssignStmtNode(VariableNode(p[1]),p[3])
 
 def p_assign_stmt_1(p):
-    'assign_stmt : ID LB expression RB ASSIGN expression'
+    'assign_stmt : ID SYM_LBRAC expression SYM_RBRAC SYM_ASSIGN expression'
     p[0] = AssignStmtNode(ArrayElementNode(p[1],[p[3]]),p[6])
 
 def p_assign_stmt_2(p):
-    'assign_stmt : ID  DOT  ID  ASSIGN  expression'
+    'assign_stmt : ID  SYM_PERIOD  ID  SYM_ASSIGN  expression'
     p[0] = AssignStmtNode(RecordElementNode(p[1],p[3]),p[5]) 
 
 def p_proc_stmt_0(p):
@@ -327,7 +327,7 @@ def p_proc_stmt_0(p):
     p[0] = VariableNode(p[1])
 
 def p_proc_stmt_1(p):
-    'proc_stmt : ID  LP  args_list  RP'
+    'proc_stmt : ID  SYM_LPAREN  args_list  SYM_RPAREN'
     p[0] = CallExprNode(p[1],p[3])
 
 def p_proc_stmt_2(p):
@@ -335,19 +335,19 @@ def p_proc_stmt_2(p):
     p[0] = p[1]
 
 def p_proc_stmt_3(p):
-    'proc_stmt : SYS_PROC  LP  expression_list  RP'
+    'proc_stmt : SYS_PROC  SYM_LPAREN  expression_list  SYM_RPAREN'
     p[0] = CallExprNode(p[1],p[3])
 
 def p_proc_stmt_4(p):
-    'proc_stmt : READ  LP  factor  RP'
+    'proc_stmt : READ  SYM_LPAREN  factor  SYM_RPAREN'###
     p[0] = CallExprNode(p[1],p[3])
 
 def p_if_stmt(p):
-    'if_stmt : IF  expression  THEN  stmt  else_clause'
+    'if_stmt : PAS_IF  expression  PAS_THEN  stmt  else_clause'
     p[0] = IfStmtNode(p[2],p[4],p[5])
 
 def p_else_clause_0(p):
-    'else_clause : ELSE stmt'
+    'else_clause : PAS_ELSE stmt'
     p[0] = p[2]
 
 def p_else_clause_1(p):
@@ -355,27 +355,27 @@ def p_else_clause_1(p):
     p[0] = None
 
 def p_repeat_stmt(p):
-    'repeat_stmt : REPEAT  stmt_list  UNTIL  expression'
+    'repeat_stmt : PAS_REPEAT  stmt_list  PAS_UNTIL  expression'
     p[0] = RepeatStmtNode(p[2],p[4])
 
 def p_while_stmt(p):
-    'while_stmt : WHILE  expression  DO stmt'
+    'while_stmt : PAS_WHILE  expression  PAS_DO stmt'
     p[0] = WhileStmtNode(p[2],p[4])
 
 def p_for_stmt(p):
-    'for_stmt : FOR  ID  ASSIGN  expression  direction  expression  DO stmt'
-    p[0] = ForStmt(p[2],p[4],p[5],p[6],p[8]) ###
+    'for_stmt : PAS_FOR  ID  SYM_ASSIGN  expression  direction  expression PAS_DO stmt'
+    p[0] = ForStmtNode(p[2],p[4],p[5],p[6],p[8]) ###
 
 def p_direction_0(p):
-    'direction : TO'
+    'direction : PAS_TO'
     p[0] = ConstValueNode('integer',1)
 
 def p_direction_1(p):
-    'direction : DOWNTO'
+    'direction : PAS_DOWNTO'
     p[0] = ConstValueNode('integer',-1)
 
 def p_case_stmt(p):
-    'case_stmt : CASE expression OF case_expr_list  END'
+    'case_stmt : PAS_CASE expression PAS_OF case_expr_list  PAS_END'
     p[0] = CaseStmtNode(p[2],p[4])
 
 def p_case_expr_list_0(p):
@@ -391,19 +391,19 @@ def p_case_expr_list_1(p):
     p[0] = CaseExprListNode(p[1])
 
 def p_case_expr_0(p):
-    'case_expr : const_value  COLON  stmt  SEMI'
+    'case_expr : const_value  SYM_COLON  stmt  SYM_SEMICOLON'
     p[0] = CaseExprNode(p[1],p[3])
 
 def p_case_expr_1(p):
-    'case_expr : ID  COLON  stmt  SEMI'
+    'case_expr : ID  SYM_COLON  stmt  SYM_SEMICOLON'
     p[0] = CaseExprNode(p[1],p[3])
 
 def p_goto_stmt(p):
-    'goto_stmt : GOTO  INTEGER'
+    'goto_stmt : PAS_GOTO  INT'
     pass###
 
 def p_expression_list_0(p):
-    'expression_list : expression_list  COMMA  expression'
+    'expression_list : expression_list  SYM_COMMA  expression'
     p[0] = p[1]
     if p[3] is not None:
         p[0].append(p[3])
@@ -413,27 +413,27 @@ def p_expression_list_1(p):
     p[0] = ExprListNode(p[1])
 
 def p_expression_0(p):
-    'expression : expression  GE  expr'
+    'expression : expression  SYM_GE  expr'
     p[0] = BinaryExprNode(p[2],p[1],p[3])
 
 def p_expression_1(p):
-    'expression : expression  GT  expr'
+    'expression : expression  SYM_GT  expr'
     p[0] = BinaryExprNode(p[2],p[1],p[3])
 
 def p_expression_2(p):
-    'expression : expression  LE  expr'
+    'expression : expression  SYM_LE  expr'
     p[0] = BinaryExprNode(p[2],p[1],p[3])
 
 def p_expression_3(p):
-    'expression : expression  LT  expr'
+    'expression : expression  SYM_LT  expr'
     p[0] = BinaryExprNode(p[2],p[1],p[3])
 
 def p_expression_4(p):
-    'expression : expression  EQUAL  expr  '
+    'expression : expression  SYM_EQ  expr  '
     p[0] = BinaryExprNode(p[2],p[1],p[3])
 
 def p_expression_5(p):
-    'expression : expression  UNEQUAL  expr'
+    'expression : expression  SYM_NE  expr'
     p[0] = BinaryExprNode(p[2],p[1],p[3])
 
 def p_expression_6(p):
@@ -441,15 +441,15 @@ def p_expression_6(p):
     p[0] = p[1]
 
 def p_expr_0(p):
-    'expr : expr  PLUS  term'
+    'expr : expr  SYM_ADD  term'
     p[0] = BinaryExprNode('+',p[1],p[3])
 
 def p_expr_1(p):
-    'expr : expr  MINUS  term'
+    'expr : expr  SYM_SUB  term'
     p[0] = BinaryExprNode('-',p[1],p[3])
 
 def p_expr_2(p):
-    'expr : expr  OR  term'
+    'expr : expr  PAS_OR  term'
     p[0] = BinaryExprNode('|',p[1],p[3])
 
 def p_expr_3(p):
@@ -457,19 +457,19 @@ def p_expr_3(p):
     p[0] = p[1]
 
 def p_term_0(p):
-    'term : term  MUL  factor'
+    'term : term  SYM_MUL  factor'
     p[0] = BinaryExprNode('*',p[1],p[3])
 
 def p_term_1(p):
-    'term : term  DIV  factor'
+    'term : term  SYM_DIV  factor'
     p[0] = BinaryExprNode('/',p[1],p[3])
 
 def p_term_2(p):
-    'term : term  MOD  factor '
+    'term : term  PAS_MOD  factor '
     p[0] = BinaryExprNode('%',p[1],p[3])
 
 def p_term_3(p):
-    'term : term  AND  factor'
+    'term : term  PAS_AND  factor'
     p[0] = BinaryExprNode('+',p[1],p[3])
 
 def p_term_4(p):
@@ -481,7 +481,7 @@ def p_factor_0(p):
     p[0] = VariableNode(p[1])
 
 def p_factor_1(p):
-    'factor : ID  LP  args_list  RP'
+    'factor : ID  SYM_LPAREN  args_list  SYM_RPAREN'
     p[0] = CallExprNode(p[1],p[3])
 
 def p_factor_2(p):
@@ -489,7 +489,7 @@ def p_factor_2(p):
     p[0] = CallExprNode(p[1],None)
 
 def p_factor_3(p):
-    'factor : SYS_FUNCT  LP  args_list  RP'
+    'factor : SYS_FUNCT  SYM_LPAREN  args_list  SYM_RPAREN'
     p[0] = CallExprNode(p[1],p[3])
 
 def p_factor_4(p):
@@ -497,31 +497,36 @@ def p_factor_4(p):
     p[0] = p[1]
 
 def p_factor_5(p):
-    'factor : LP  expression  RP'
+    'factor : SYM_LPAREN  expression  SYM_RPAREN'
     p[0] = p[2]
 
 def p_factor_6(p):
-    'factor : NOT  factor'
+    'factor : PAS_NOT  factor'
     p[0] = UnaryExprNode('!',p[2])
 
 def p_factor_7(p):
-    'factor : MINUS  factor'
+    'factor : SYM_SUB  factor'
     p[0] = UnaryExprNode('-',p[2])
 
 def p_factor_8(p):
-    'factor : ID  LB  expression  RB'
+    'factor : ID  SYM_LBRAC  expression  SYM_RBRAC'
     p[0] = ArrayElementNode(p[1],[p[3]])
 
 def p_factor_9(p):
-    'factor : ID  DOT  ID'
+    'factor : ID  SYM_PERIOD  ID'
     p[0] = RecordElementNode(p[1],p[3])
 
 def p_args_list_0(p):
-    'args_list : args_list  COMMA  expression'
+    'args_list : args_list  SYM_COMMA  expression'
     p[0] = p[1]
     p[0].append(p[3])
 
 def p_args_list_1(p):
     'args_list : expression'
     p[0] = ArgsListNode(p[1])
-    
+
+def p_empty(p):
+    'empty : '
+    p[0] = None
+
+parser=yacc.yacc(debug=True,debuglog=log)
