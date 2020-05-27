ec86474430522b5bf3ed6cc409cd822c926ee4a0
WuYiqian <3170104915@zju.edu.cn>
2020-05-27 16:45:16 +0800

    add optimization

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 from PasParser.parser import parser
 from PasGenerator.codegenerator import *
 import llvmlite.binding as llvm
-
+import argparse
+arg = argparse.ArgumentParser()
+arg.add_argument("--input_file", help='specify the input file',default='test_case/test_for.pas',required=False)
+arg.add_argument( "--opt_level",
+                        type=int, help="specify the optimization level",default=1,required=False)
 if __name__ == '__main__':
+    args=arg.parse_args()
     # All these initializations are required for code generation!
     llvm.initialize()
     llvm.initialize_native_target()
     llvm.initialize_native_asmprinter()  # yes, even this one
 
-    codestr = open('test.pas', 'r').read()
+    codestr = open(args.input_file, 'r').read()
     ast = parser.parse(codestr)
 
     print("====================AST====================")
@@ -29,6 +34,16 @@ if __name__ == '__main__':
     codegen = CodeGenerator('Test')
     codegen.generate(ast)
     llvm_ir = str(codegen.module)
+    llvmmod = llvm.parse_assembly(llvm_ir)
+    if args.opt_level:
+        level = args.opt_level
+        if level > 0:
+            pmb = llvm.create_pass_manager_builder()
+            pmb.opt_level = level
+            pm = llvm.create_module_pass_manager()
+            pmb.populate(pm)
+            pm.run(llvmmod)
+            llvm_ir = str(llvmmod)
 
     print("====================IR====================")
     f_ir = open('ir_code.txt', 'w')
@@ -36,7 +51,7 @@ if __name__ == '__main__':
     print(llvm_ir)
     print("====================IR====================")
 
-    llvmmod = llvm.parse_assembly(llvm_ir)
+
     target = llvm.Target.from_default_triple()
     target_machine = target.create_target_machine()
     asm = target_machine.emit_assembly(llvmmod)
